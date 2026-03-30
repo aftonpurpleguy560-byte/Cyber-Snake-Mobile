5f0d237a5d9ef2b376307a87960b47c4c9d61946
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-03-30 21:33:45 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import './index.css'
 
+// React 18+ için standart başlatma kodu
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
-
