c167a1d9f21838303c0f7de82c3833f6a5af24a3
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-03-30 21:34:06 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-import CyberSnake from './components/CyberSnake';
+import React from 'react';
+import CyberSnake from './components/CyberSnake.jsx';
 import './index.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="w-full min-h-screen bg-[#050505] flex items-center justify-center">
+      {/* Oyun Bileşeni */}
       <CyberSnake />
     </div>
   );
 }
 
 export default App;
-
