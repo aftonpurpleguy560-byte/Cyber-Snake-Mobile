5333f538213308c6df20f14a561ff7b5ca19c8a1
aftonpurpleguy560-byte <aftonpurpleguy560@gmail.com>
2026-03-30 21:23:17 +0300

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import CyberSnake from './components/CyberSnake';
+import './index.css';
+
+function App() {
+  return (
+    <div className="App">
+      <CyberSnake />
+    </div>
+  );
+}
+
+export default App;
+
